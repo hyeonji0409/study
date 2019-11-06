0630c16d3d133115842e68b81ba31342a832008e
hyeonji0409 <guswl8507@naver.com>
2019-11-06 09:35:13 +0900

    hotfix 학번 추가

--- Information/src/Information.java
+++ Information/src/Information.java
@@ -4,7 +4,7 @@ public class Information {
 	public static void main(String[] args) {
 		System.out.println("Computer SW");
 		System.out.println("Wonkwang University");
-
+		System.out.println("Nunber(20183274)");
 	}
 
 }
